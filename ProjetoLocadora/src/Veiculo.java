3e0aea48538769f0885de7ee7d7137bb9944dc80
jhnrq <fourgatti@gmail.com>
2026-04-09 21:50:32 -0300

    feat: trabalhando com a locadora de veiculos e criando as classes e trabalhando com POO

---
+++ ProjetoLocadora/src/Veiculo.java
@@ -0,0 +1,79 @@
+// Sistema de veículos - 4 Pilares da POO em java
+// Uma locadora de veículos precisa calcular o valor da diária
+// De diferentes tipos de veículos (carro e moto)
+// Cada tipo tem seu proprio calculo
+
+// Pilar 1 - ABSTRAÇÃO
+// Modelar apenas o que importa: marca, modelo, ano, placa, preço base
+// Metodo calcularDiaria() abstrato, pois cada vículo tera sua propria diaria (calculo)
+public abstract class Veiculo {
+    private String marca;
+    private String modelo;
+    private int ano;
+    private String placa;
+    private double precoBase;
+
+    public Veiculo(String marca, String modelo, int ano, String placa, double precoBase){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.placa = placa;
+        this.precoBase = precoBase;
+    }
+
+    // Metodo abstrato - cada subtipo define o calculo
+    public abstract double calcularDiaria();
+
+    // Pilar 2 - ENCAPSULAMENTO
+    // MODIFICADORES VIA GETTERS E SETTERS
+    // O SETTER PREÇOBASE TEM VALIDAÇÃO: NÃO ACEITA VALORES NEGATIVOS
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public String getPlaca(){
+        return placa;
+    }
+
+    public int getAno(){
+        return ano;
+    }
+
+    public double getPrecoBase(){
+        return precoBase;
+    }
+
+    public void setPrecoBase(double precoBase){
+        if(precoBase < 0){
+            System.out.println("Erro: preço não pode ser negativo.");
+            return;
+        }
+
+        this.precoBase = precoBase;
+    }
+
+    public String toString(){
+        return marca + " " + modelo + " (" + ano + ")";
+    }
+}
+
+// PILAR 3 - HERANÇA
+// Carro e moto herdam tudo de Veiculo, mas cada um adiciona seus proprios atributos
+// e implementa calcularDiaria() do seu jeito.
+
+// Carro -> qtdPortas (atributo)
+// metodo calcularDiaria -> se qtdPortas >=4 acrescimo de 20%
+
+abstract class Carro extends Veiculo {
+    private int qtdPortas;
+
+    public Carro(String marca, String modelo, int ano, String placa, double precoBase, int qtdPortas){
+        super(marca, modelo, ano, placa, precoBase);
+        this.qtdPortas = qtdPortas;
+    }
+}
